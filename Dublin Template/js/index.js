cb1cd353aa52274831e1d90d54f0bd2f756f5dff
larissasoarre <larylia.soaress2@gmail.com>
2022-08-30 08:29:49 -0300

    Fix of bugs

--- Dublin Template/js/index.js
+++ Dublin Template/js/index.js
@@ -1,6 +1,22 @@
-const hamburger = document.getElementsByClassName('hamburger') [0]
-const navLinks = document.getElementsByClassName('nav-links')[0]
+// const hamburger = document.getElementsByClassName('hamburger') [0]
+// const navLinks = document.getElementsByClassName('nav-links')[0]
 
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('active')
-})
+// hamburger.addEventListener('click', () => {
+//     navLinks.classList.toggle('active')
+// })
+
+
+// -------test 
+
+//Navbar Hamburger
+
+window.onload = function () {
+    const menuButton = document.querySelector('.hamburger');
+    const mobileMenu = document.querySelector('.mobile-nav-container');
+
+    menuButton.addEventListener('click', function () {
+        menuButton.classList.toggle('is-active');
+        mobileMenu.classList.toggle('is-active');
+
+    })
+}
