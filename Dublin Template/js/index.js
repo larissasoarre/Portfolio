9b8ad388655018d7fd43afa52993d6f5f19c58f1
larissasoarre <larylia.soaress2@gmail.com>
2022-08-30 16:18:24 -0300

    Fixing of bugs

--- Dublin Template/js/index.js
+++ Dublin Template/js/index.js
@@ -1,13 +1,3 @@
-// const hamburger = document.getElementsByClassName('hamburger') [0]
-// const navLinks = document.getElementsByClassName('nav-links')[0]
-
-// hamburger.addEventListener('click', () => {
-//     navLinks.classList.toggle('active')
-// })
-
-
-// -------test 
-
 //Navbar Hamburger
 
 window.onload = function () {
